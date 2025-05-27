2d6d0f6b6c1519441d1a90455fa0579d2ca3a394
sofiasilva <sofia.silva26@escola.pr.gov.br>
2025-05-27 16:01:53 -0300

    Add files via upload

---
+++ Sofia.2t.2b-main/perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Supernatural',
+    'Qual o nome dos dois irmãos protagonistas da série?',
+    'Sam e Dean Winchester'
+)
+
+criaCartao(
+    'Supernatural',
+    'Qual o nome do carro que Dean dirige?',
+    'Impala 67 (No Brasil também é conhecido como Opala 67) '
+)
+
+criaCartao(
+    'Supernatural',
+    'Qual criatura os irmãos caçam no primeiro episódio da série?',
+    'Mulher de Branco'
+)
+
+criaCartao(
+    'Supernatural',
+    'Quem é o anjo que ajuda os irmãos em várias temporadas?',
+    'Castiel'
+)
+
+criaCartao(
+    'Supernatural',
+    'Qual é o apelido que Dean dá ao anjo Castiel?',
+    'Cas'
+)
+
+criaCartao(
+    'Supernatural',
+    'Qual é o nome do pai dos irmãos Winchester?',
+    'John Winchester'
+
+)
+
+criaCartao(
+    'Supernatural',
+    'Em que canal a série foi originalmente exibida nos Estados Unidos?',
+    'The CW'
+)
+
+criaCartao(
+    'Supernatural',
+    'Qual o nome do demônio que possui olhos amarelos?',
+    'Azazel'
+)
+
+criaCartao(
+    'Supernatural',
+    'Quantas temporadas tem a série Supernatural?',
+    '15 temporadas'
+)
